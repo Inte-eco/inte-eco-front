8da2e187fc33df2257e6cf3a280503e6d80cf3ce
Josh Muleshi <jmuleshi2@gmail.com>
2025-07-17 15:52:12 -0400

    bug not resolved

--- src/services/Firebase/FirebaseConfig.tsx
+++ src/services/Firebase/FirebaseConfig.tsx
@@ -3,24 +3,24 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDN1oovfjfTQaGu7OczF8X2lBBoVu4axJc",
-//   authDomain: "inte-eco-cd.firebaseapp.com",
-//   projectId: "inte-eco-cd",
-//   storageBucket: "inte-eco-cd.firebasestorage.app",
-//   messagingSenderId: "1059137775482",
-//   appId: "1:1059137775482:web:350fd925572439e85158c3",
-//   // measurementId: import.meta.env.VITE_MEASUREMENT_ID
-// };
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyDN1oovfjfTQaGu7OczF8X2lBBoVu4axJc",
+  authDomain: "inte-eco-cd.firebaseapp.com",
+  projectId: "inte-eco-cd",
+  storageBucket: "inte-eco-cd.firebasestorage.app",
+  messagingSenderId: "1059137775482",
+  appId: "1:1059137775482:web:350fd925572439e85158c3",
+  // measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_ID,
+// };
 
 // Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
